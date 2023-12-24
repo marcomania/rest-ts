2e67d508401f117aa2f090821f021205451f4992
Marco Untiveros <marco.untiveros@unmsm.edu.pe>
2023-12-23 21:20:33 -0500

    first commit

---
+++ src/main/java/dev/unmsm/restts/RestTsApplication.java
@@ -0,0 +1,13 @@
+package dev.unmsm.restts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestTsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestTsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/unmsm/restts/RestTsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.unmsm.restts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestTsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
